5222db48c973b53a439ee232a19c2bd3e023dbe3
melerodaw <melerodaw@gmail.com>
2026-05-11 12:10:07 +0200

    Coordinar .java con .json

--- src/proyectogrupal/Accion.java
+++ src/proyectogrupal/Accion.java
@@ -1,23 +1,35 @@
 package proyectogrupal;
 
 public class Accion{
+    private int id;
     private String descripcionCarta;
 
-    public Accion() {}
+    public Accion() {
+        this.id = 0;
+    }
 
-    public Accion(String descripcionCarta) {
+    public Accion(int id, String descripcionCarta) {
+        this.id = id;
         this.descripcionCarta = descripcionCarta;
     }
 
+    public Accion(String descripcionCarta) {
+        this(0, descripcionCarta);
+    }
+
     public Accion(Accion a) {
+        this.id = a.id;
         this.descripcionCarta = a.descripcionCarta;
     }
 
+    public int getId() { return id; }
+
     public String getDescripcionCarta() { return descripcionCarta; }
+    public void setId(int id) { this.id = id; }
     public void setDescripcionCarta(String descripcionCarta) { this.descripcionCarta = descripcionCarta; }
 
     @Override
     public String toString() {
-        return "Accion{descripcionCarta='" + descripcionCarta + "'}";
+        return "Accion{id=" + id + ", descripcionCarta='" + descripcionCarta + "'}";
     }
 }
--- src/proyectogrupal/Carta.java
+++ src/proyectogrupal/Carta.java
@@ -12,30 +12,51 @@ package proyectogrupal;
 public class Carta {
  
     // Atributos
+    private int id;
     private String descripcion;
+    private String tipo;
     private int importe;
  
     // Constructor por defecto
     public Carta() {
+        this.id = 0;
         this.descripcion = "Sin descripcion";
+        this.tipo = "SIN_TIPO";
         this.importe = 0;
     }
  
     // Constructor por parametros
-    public Carta(String descripcion, int importe) {
+    public Carta(int id, String descripcion, String tipo, int importe) {
+        this.id = id;
         this.descripcion = descripcion;
+        this.tipo = tipo;
         this.importe = importe;
+    }
+
+    // Constructor compatible con el modelo anterior
+    public Carta(String descripcion, int importe) {
+        this(0, descripcion, "SUERTE", importe);
     }
  
     // Constructor de copia
     public Carta(Carta c) {
+        this.id = c.id;
         this.descripcion = c.descripcion;
+        this.tipo = c.tipo;
         this.importe = c.importe;
     }
  
     // Getters
+    public int getId() {
+        return this.id;
+    }
+
     public String getDescripcion() {
         return this.descripcion;
+    }
+
+    public String getTipo() {
+        return this.tipo;
     }
  
     public int getImporte() {
@@ -43,8 +64,16 @@ public class Carta {
     }
  
     // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
  
     public void setImporte(int importe) {
@@ -54,6 +83,6 @@ public class Carta {
     // toString
     @Override
     public String toString() {
-        return "Carta [descripcion=" + descripcion + ", importe=" + importe + "]";
+        return "Carta [id=" + id + ", descripcion=" + descripcion + ", tipo=" + tipo + ", importe=" + importe + "]";
     }
 }
